7acd4ffb5d5f10f72d261be7ad2a39ec7b554557
Yunfei <yunfeitang743@gmail.com>
2023-09-09 21:14:44 -0400

    revise7 according to autograder

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -17,7 +17,7 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
-        if (array.length < size * 2) {
+        if (array.length == size) {
             resize(size * 2);
         }
         System.arraycopy(array, 0, array, 1, size);
@@ -29,7 +29,7 @@ public class ArrayDeque<T> {
      * Inserts X into the back of the list.
      */
     public void addLast(T item) {
-        if (size > array.length / 2) {
+        if (size == array.length) {
             resize(size * 2);
         }
         array[size] = item;
@@ -61,8 +61,8 @@ public class ArrayDeque<T> {
         System.arraycopy(array, 1, array, 0, size - 1);
         array[size - 1] = null;
         size = size - 1;
-        if (size < array.length / 4) {
-            resize(array.length / 4);
+        if (size < array.length / 2) {
+            resize(array.length / 2);
         }
         return x;
     }
@@ -78,8 +78,8 @@ public class ArrayDeque<T> {
         T x = array[size - 1];
         array[size - 1] = null;
         size = size - 1;
-        if (size < array.length / 4) {
-            resize(array.length / 4);
+        if (size < array.length / 2) {
+            resize(array.length / 2);
         }
         return x;
     }
