25cb516de2928ab609fbcc01de700196a8b08f38
Yunfei <yunfeitang743@gmail.com>
2023-09-09 21:05:47 -0400

    revise6 according to autograder

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -17,13 +17,10 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
-        if (array.length == size) {
-            T[] a = (T[]) new Object[size * 2];
-            System.arraycopy(array, 0, a, 1, size);
-            array = a;
-        } else {
-            System.arraycopy(array, 0, array, 1, size);
+        if (array.length < size * 2) {
+            resize(size * 2);
         }
+        System.arraycopy(array, 0, array, 1, size);
         array[0] = item;
         size = size + 1;
     }
@@ -32,7 +29,7 @@ public class ArrayDeque<T> {
      * Inserts X into the back of the list.
      */
     public void addLast(T item) {
-        if (size == array.length) {
+        if (size > array.length / 2) {
             resize(size * 2);
         }
         array[size] = item;
@@ -61,12 +58,12 @@ public class ArrayDeque<T> {
             return null;
         }
         T x = array[0];
-        if (size < array.length / 4) {
-            T[] a = (T[]) new Object[array.length / 2];
-            System.arraycopy(array, 1, a, 0, size - 1);
-            array = a;
-        }
+        System.arraycopy(array, 1, array, 0, size - 1);
+        array[size - 1] = null;
         size = size - 1;
+        if (size < array.length / 4) {
+            resize(array.length / 4);
+        }
         return x;
     }
 
@@ -79,11 +76,11 @@ public class ArrayDeque<T> {
             return null;
         }
         T x = array[size - 1];
-        if (size < array.length / 4) {
-            resize(array.length / 2);
-        }
         array[size - 1] = null;
         size = size - 1;
+        if (size < array.length / 4) {
+            resize(array.length / 4);
+        }
         return x;
     }
 
